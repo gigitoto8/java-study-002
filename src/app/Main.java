39f20cc67723d530ba98e2943a8839a7402c0f26
gigitoto8 <gradu2a8nge@aol.com>
2026-04-19 22:10:11 +0900

    CSV化、Main内整理

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,8 @@
 package app;
 
 import app.model.Task;
+import app.model.TaskLog;
+import app.repository.TaskLogRepository;
 import app.repository.TaskRepository;
 import app.service.TaskLogService;
 import app.service.TaskService;
@@ -8,70 +10,115 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Main{
+
+    private static TaskService tService;
+    private static TaskLogService tLService;
     
     public static void main(String[] args) {
-        String task;
-        String category;
-        String date;
-        int minutes;
-        String memo;
-        int taskId;
-
-        //入力関係
-        Scanner sc = new Scanner(System.in,StandardCharsets.UTF_8);
-        
-        //TaskRepositoryクラス用
+        // 初期化ブロック
+        init();
+        // 処理ブロック
+        run();
+    }
+    
+    static void init(){
+        // Repository生成（CSVファイルへの読み書きを担当）
         TaskRepository tRepository = new TaskRepository();
-        // TaskRepositoryをTaskServiceに渡すことで、
-        // タスク追加時に「メモリへの登録」と「CSVへの保存」を一貫して行えるようにする
-        TaskService tService = new TaskService(tRepository);
-        // TaskServiceをTaskLogServiceに渡すことで、
-        // TaskLog登録時にtaskIdの存在チェックを行う（同一のTaskデータを参照するため）
-        TaskLogService tLService = new TaskLogService(tService);
-        
-        System.out.println("----------------------------------------");
-        
+        TaskLogRepository tLRepository = new TaskLogRepository();
+        // service生成
+        // TaskRepositoryを渡して依存関係を構築
+        tService = new TaskService(tRepository);
+        // TaskLogRepositoryを渡して依存関係を構築
+        // taskId存在チェックのため、TaskServiceも必要
+        tLService = new TaskLogService(tService,tLRepository);
+        // CSVからデータ読み込み、Listに復元
         tService.loadTaskCSV();
-
-        System.out.print("task ? : ");
-        task = sc.nextLine();
-        System.out.print("category ? : ");
-        category = sc.nextLine();
-        Task task001 = new Task(task, category);
-
-        //taskリストに加えると同時に、CSVファイルに保存
-        tService.addTask(task001);
-        /*
-        */
-        //↓動作確認で入力時間を省くため、オーバーロードで対処
-        tService.addTask("買い物","家事");
-        tService.addTask("書類作成","仕事");
-        //taskリスト一覧表示およびCSV保存
-        tService.getTasks();
-
+        tLService.loadTaskLogCSV();
+    }
+    
+    static void run(){
         
-        /*
+        System.out.println("\n----------------------------------------");
+        System.out.println("-----タスク管理アプリ----");
+        System.out.println("----------------------------------------\n\n");
+
+        Scanner sc = new Scanner(System.in,StandardCharsets.UTF_8);
+        while (true) { 
+            System.out.println("----------------------------------------");
+            
+            System.out.println("実行したい機能を選択してください");
+            System.out.println(" 1 : タスク登録 / 2 : ログ登録 / " + 
+                        "3 : タスク一覧表示 / 4 : ログ一覧表示 / 5 : 終了\n");
+
+            //整数以外を入力した場合の処理
+            if(!sc.hasNextInt()){
+                System.out.println("数値を入力してください");
+                sc.nextLine();
+                continue;
+            }
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                // 1 : タスク登録
+                case 1:
+                    Task tInput = inputTask(sc);
+                    tService.addTask(tInput);
+                    // ↓サンプル入力の手間を省くため、オーバーロードで対処
+                    tService.addTask("買い物","家事");
+                    tService.addTask("書類作成","仕事");                    
+                    break;
+                                
+                // 2 : ログ登録
+                case 2:
+                    TaskLog tLInput = inputTaskLog(sc);
+                    tLService.addTaskLog(tLInput);
+                    // ↓サンプル入力の手間を省くため、オーバーロードで対処
+                    tLService.addTaskLog(2,"2026-01-31",60,"洗濯");
+                    tLService.addTaskLog(3,"2026-02-15",120,"帳簿");        
+                    tLService.addTaskLog(4,"2020-10-15",300,"買い物");        
+                break;
+
+                // 3 : タスク一覧表示
+                case 3:
+                    tService.getTasks();
+                break;
+                
+                // 4 : ログ一覧表示
+                case 4:
+                    tLService.getTaskLogs();
+                break;
+                
+                // 5 : 終了
+                case 5 :
+                    System.out.println("終了します");
+                return;
+
+                default:
+                    System.out.println("入力が不正です。");
+            }            
+        }
+    }
+
+    static Task inputTask(Scanner sc){
+        System.out.print("task ? : ");
+        String task = sc.nextLine();
+        System.out.print("category ? : ");
+        String category = sc.nextLine();            
+        return new Task(task,category);
+    }
+    
+    static TaskLog inputTaskLog(Scanner sc){
         System.out.print("date ? : ");
-        date = sc.nextLine();
+        String date = sc.nextLine();
         System.out.print("minutes ? : ");
-        minutes = sc.nextInt();
+        int minutes = sc.nextInt();
         sc.nextLine();
         System.out.print("memo ? : ");
-        memo = sc.nextLine();
+        String memo = sc.nextLine();
         System.out.print("ID ? : ");
-        taskId = sc.nextInt();
-        
-        TaskLog taskLog001 = new TaskLog(taskId ,date ,minutes, memo);
-        //tasklogリストに加える
-        tLService.addTaskLog(taskLog001);
-        //↓動作確認で入力時間を省くため、オーバーロードで対処
-        tLService.addTaskLog(2,"2026-01-31",60,"洗濯");
-        tLService.addTaskLog(3,"2026-02-15",120,"帳簿");        
-        tLService.addTaskLog(4,"2020-10-15",300,"買い物");        
-        //tasklogリスト一覧表示
-        for(TaskLog t : tLService.getTaskLogs()){
-            System.out.println(t);
-        }        
-        */
+        int taskId = sc.nextInt();
+        sc.nextLine();
+        return new TaskLog(taskId,date,minutes,memo);
     }
 }
